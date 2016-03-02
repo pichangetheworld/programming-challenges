11eb4be39b0a23747ea6b4bf12cada09a319c399
P Chan <shiroyamaryu@gmail.com>
2016-03-03 08:17:02 +0900

    Adding helpful snackbar

--- app/src/main/java/com/pichangetheworld/programmingchallenges/MainActivity.java
+++ app/src/main/java/com/pichangetheworld/programmingchallenges/MainActivity.java
@@ -3,8 +3,6 @@ package com.pichangetheworld.programmingchallenges;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -20,27 +18,5 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         recyclerView.setAdapter(new ProblemAdapter(this));
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/pichangetheworld/programmingchallenges/ProblemActivity.java
+++ app/src/main/java/com/pichangetheworld/programmingchallenges/ProblemActivity.java
@@ -3,13 +3,26 @@ package com.pichangetheworld.programmingchallenges;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Base Activity class
  */
 public class ProblemActivity extends AppCompatActivity {
     private static final String EXTRA_PROBLEM_ENUM = "problemEnum";
+
+    @Bind(android.R.id.content)
+    FrameLayout contentView;
+
+    Problem problem;
 
     public static Intent createIntent(Context context, Problem problem) {
         Intent intent = new Intent(context, ProblemActivity.class);
@@ -20,11 +33,40 @@ public class ProblemActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ButterKnife.bind(this);
 
-        Problem problem = (Problem) getIntent().getSerializableExtra(EXTRA_PROBLEM_ENUM);
+        problem = (Problem) getIntent().getSerializableExtra(EXTRA_PROBLEM_ENUM);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle(problem.getTitle());
+        }
 
         getSupportFragmentManager().beginTransaction()
                 .replace(android.R.id.content, problem.getFragment())
                 .commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            case R.id.action_help:
+                Snackbar.make(contentView, problem.getDescription(), Snackbar.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
