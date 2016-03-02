0b1833b24c0372bf5dca2ef7ac623b3d8ed6e478
P Chan <shiroyamaryu@gmail.com>
2016-03-02 17:18:45 +0900

    Adding diff finder

--- app/src/main/java/com/pichangetheworld/programmingchallenges/DiffFinder.java
+++ app/src/main/java/com/pichangetheworld/programmingchallenges/DiffFinder.java
@@ -8,10 +8,22 @@ import java.util.List;
 public class DiffFinder implements DiffFinderInterface {
     /*
      * Returns a list of strings that are present in listB but not in listA
+     *
+     * e.g. If ListA = {"one", "three"}, ListB = {"one", "two", "three", "four", "five"}
+     *      --> return {"two", "four", "five"}
+     *
+     * e.g. If ListA = {"ken", "nanri"}, ListB = {"bon", "ken", "nanri", "okada", "ryu"}
+     *      --> return {"bon", "okada", "ryu"}
+     *
+     * e.g. If ListA = {"aaa"}, ListB = {"aaa"}
+     *      --> return {} <- empty list
+     *
+     * e.g. If ListA = {}, ListB = {"one", "two", "three", "four", "five"}
+     *      --> return {"one", "two", "three", "four", "five"}
      */
     @Override
     public List<String> findDiff(List<String> listA, List<String> listB) {
-        // TODO
+        // TODO implement this
         return listB;
     }
 }
